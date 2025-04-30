6fd3227915bba8a8760d1d966d8f81a8aab017a7
Aymen Ajroudi <aymen.ajroudi2020@gmail.com>
2025-04-30 23:02:29 +0200

    Update Credential

--- controllers/MailerController.js
+++ controllers/MailerController.js
@@ -5,15 +5,15 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     service: 'gmail', // Peut être remplacé par un autre service comme Outlook, Yahoo, etc.
     auth: {
-        user: 'aymen.ajroudi2020@gmail.com', // Adresse e-mail
-        pass: 'ulxu ryyn reuk swpz'       // Mot de passe d'application généré par le service d'email
+        user: 'exemple@gmail.com', // Adresse e-mail
+        pass: 'swpz swpz swpz swpz'       // Mot de passe d'application généré par le service d'email
     }
 });
 
 // Définis les options de l'e-mail
 let mailOptions = {
-    from: 'aymen.ajroudi2020@gmail.com',         // L'expéditeur
-    to: 'ajroudi@et.esiea.fr',     // Destinataire(s)
+    from: 'exemple@gmail.com',         // L'expéditeur
+    to: 'exemple@et.client.fr',     // Destinataire(s)
     subject: 'Test d\'envoi de mail avec Node.js', // Sujet de l'e-mail
     text: 'Salut ! Ceci est un test d\'envoi de mail avec Nodemailer.', // Contenu en texte brut
     html: '<b>Salut !</b> Ceci est un test d\'envoi de mail avec Nodemailer.' // Contenu en HTML
